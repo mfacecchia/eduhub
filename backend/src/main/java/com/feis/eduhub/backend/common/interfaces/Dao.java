2ecfa8ca4119ab61ef57b75ed0ef74326a21cdec
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-23 18:40:37 +0100

    refactor: update generic dao layer interface methods

--- backend/src/main/java/com/feis/eduhub/backend/common/interfaces/Dao.java
+++ backend/src/main/java/com/feis/eduhub/backend/common/interfaces/Dao.java
@@ -1,15 +1,16 @@
 package com.feis.eduhub.backend.common.interfaces;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface Dao<T> {
     T create(T obj);
 
-    T findById(long id);
+    Optional<T> findById(long id);
 
     void update(long id, T obj);
 
-    boolean delete(long id);
+    void delete(long id);
 
     List<T> getAll();
 }
