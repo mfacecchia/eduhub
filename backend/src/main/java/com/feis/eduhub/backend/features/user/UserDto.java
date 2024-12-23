7714b9ea535cafc1a7c449ac19530fa3b21fe8f1
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-23 21:24:32 +0100

    refactor: remove roleId

--- backend/src/main/java/com/feis/eduhub/backend/features/user/UserDto.java
+++ backend/src/main/java/com/feis/eduhub/backend/features/user/UserDto.java
@@ -15,9 +15,8 @@ import lombok.NoArgsConstructor;
  * @param firstName User's first name
  * @param lastName  User's last name
  * @param email     User's email address
- * @param icon      Path or URL to user's profile icon/avatar
- * @param roleName  Name of the user's role in the system
- * @param roleId    Unique identifier for the user's role
+ * @param icon      Path or URL to user's profile icon
+ * @param roleName  Name of the user's role in the database
  * 
  * @see Credentials
  * @see Account
@@ -33,23 +32,4 @@ public class UserDto {
     private String email;
     private String icon;
     private String roleName;
-    private Long roleId;
-
-    public UserDto(Long accountId, String firstName, String lastName, String email, String icon, String roleName) {
-        this.accountId = accountId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.icon = icon;
-        this.roleName = roleName;
-    }
-
-    public UserDto(Long accountId, String firstName, String lastName, String email, String icon, Long roleId) {
-        this.accountId = accountId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.icon = icon;
-        this.roleId = roleId;
-    }
 }
