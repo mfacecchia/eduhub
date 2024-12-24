2977dd8fb8f7372865fb2400364432553b54cd15
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-24 12:38:19 +0100

    feat: add JWT service layer class

---
+++ backend/src/main/java/com/feis/eduhub/backend/common/auth/lib/JwtService.java
@@ -0,0 +1,53 @@
+package com.feis.eduhub.backend.common.auth.lib;
+
+import com.feis.eduhub.backend.common.config.RedisConnection;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
+
+public class JwtService {
+    private final RedisConnection redisConnection = RedisConnection.getInstance();
+    private final JwtDao jwtDao = new JwtDao();
+
+    public void storeJwt(String jti, String accountId, long exp) {
+        try (Jedis conn = redisConnection.getConnection()) {
+            SetParams setParams = new SetParams();
+            if (exp > 0) {
+                setParams.exAt(exp);
+            }
+            jwtDao.create(jti, accountId, setParams, conn);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void deleteJwt(String jti) {
+        try (Jedis conn = redisConnection.getConnection()) {
+            jwtDao.delete(jti, conn);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public String getJwt(String jti) {
+        try (Jedis conn = redisConnection.getConnection()) {
+            String result = jwtDao.findByKey(jti, conn);
+            return result;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void updateJwt(String jti, String accountId, long exp) {
+        try (Jedis conn = redisConnection.getConnection()) {
+            SetParams setParams = new SetParams();
+            setParams.xx();
+            if (exp > 0) {
+                setParams.exAt(exp);
+            }
+            jwtDao.update(jti, accountId, setParams, conn);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
