8ff344bfc18932be28ae4f86915fc32d01b98725
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-24 16:21:12 +0100

    feat: implement auth routes in main app

--- backend/src/main/java/com/feis/eduhub/backend/App.java
+++ backend/src/main/java/com/feis/eduhub/backend/App.java
@@ -1,10 +1,13 @@
 package com.feis.eduhub.backend;
 
+import java.util.Arrays;
+
+import com.feis.eduhub.backend.common.auth.controller.AuthController;
 import com.feis.eduhub.backend.common.config.AppBuilder;
 
 public class App {
     public static void main(String[] args) {
-        AppBuilder appBuilder = new AppBuilder(null);
+        AppBuilder appBuilder = new AppBuilder(Arrays.asList(new AuthController()));
         appBuilder.configureEndpoints(true);
         appBuilder.start();
     }
