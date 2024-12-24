5511fef3649b555e71149d726ae8c364ba904200
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-24 19:02:33 +0100

    refactor: replace RuntimeException with Exception

--- backend/src/main/java/com/feis/eduhub/backend/common/exceptions/AppException.java
+++ backend/src/main/java/com/feis/eduhub/backend/common/exceptions/AppException.java
@@ -5,7 +5,7 @@ package com.feis.eduhub.backend.common.exceptions;
  * This exception includes an HTTP status code along with the error message for
  * useful erorr logging and response generation.
  */
-public class AppException extends RuntimeException {
+public class AppException extends Exception {
     private final int statusCode;
 
     public AppException(int statusCode, String message) {
