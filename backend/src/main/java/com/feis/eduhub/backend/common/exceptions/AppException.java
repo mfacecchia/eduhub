cb4ce32cd0eb2037c263f92221f1a4d3375780e8
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-24 18:59:03 +0100

    feat: add custom app exception class

---
+++ backend/src/main/java/com/feis/eduhub/backend/common/exceptions/AppException.java
@@ -0,0 +1,24 @@
+package com.feis.eduhub.backend.common.exceptions;
+
+/**
+ * Custom generic App Exception for application-specific errors handling.
+ * This exception includes an HTTP status code along with the error message for
+ * useful erorr logging and response generation.
+ */
+public class AppException extends RuntimeException {
+    private final int statusCode;
+
+    public AppException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public AppException(int statusCode, String message, Throwable cause) {
+        super(message, cause);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
