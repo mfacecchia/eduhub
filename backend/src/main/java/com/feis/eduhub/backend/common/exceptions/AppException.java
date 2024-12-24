e3852a5f8eb237a52bb68436916ac09c4bba92f3
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-24 19:24:47 +0100

    chore: update javadoc

--- backend/src/main/java/com/feis/eduhub/backend/common/exceptions/AppException.java
+++ backend/src/main/java/com/feis/eduhub/backend/common/exceptions/AppException.java
@@ -2,8 +2,12 @@ package com.feis.eduhub.backend.common.exceptions;
 
 /**
  * Custom generic App Exception for application-specific errors handling.
- * This exception includes an HTTP status code along with the error message for
+ * This exception includes an HTTP status code along with an error message in a
+ * user friendly format (e.g. the more generic the better) for
  * useful erorr logging and response generation.
+ * 
+ * It is also possible to pass a {@code Throwable} cause representing the
+ * exception that thrown the custom App Exception.
  */
 public class AppException extends Exception {
     private final int statusCode;
