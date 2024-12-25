4bbc8e7d2707b6af9408e425986f491857b3e061
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-25 10:38:56 +0100

    feat: add lesson POJO

---
+++ backend/src/main/java/com/feis/eduhub/backend/features/lesson/Lesson.java
@@ -0,0 +1,47 @@
+package com.feis.eduhub.backend.features.lesson;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Lesson {
+    @NonNull
+    private Long lessonId;
+    @NonNull
+    private Date lessonDate;
+    @NonNull
+    private Time startsAt;
+    @NonNull
+    private Time endsAt;
+    @NonNull
+    private Integer roomNo;
+    private Long accountId;
+    @NonNull
+    private Long classId;
+
+    public Lesson(@NonNull Date lessonDate, @NonNull Time startsAt, @NonNull Time endsAt, @NonNull Integer roomNo,
+            @NonNull Long classId) {
+        this.lessonDate = lessonDate;
+        this.startsAt = startsAt;
+        this.endsAt = endsAt;
+        this.roomNo = roomNo;
+        this.classId = classId;
+    }
+
+    public Lesson(@NonNull Date lessonDate, @NonNull Time startsAt, @NonNull Time endsAt, @NonNull Integer roomNo,
+            Long accountId, @NonNull Long classId) {
+        this.lessonDate = lessonDate;
+        this.startsAt = startsAt;
+        this.endsAt = endsAt;
+        this.roomNo = roomNo;
+        this.accountId = accountId;
+        this.classId = classId;
+    }
+}
