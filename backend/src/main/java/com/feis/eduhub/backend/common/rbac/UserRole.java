cf9f2b8db85283e62e68b141ad24c999d0e776b1
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-26 18:37:00 +0100

    feat(rbac): add roles permissions enumeration

---
+++ backend/src/main/java/com/feis/eduhub/backend/common/rbac/UserRole.java
@@ -0,0 +1,21 @@
+package com.feis.eduhub.backend.common.rbac;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+public enum UserRole {
+    ADMIN(EnumSet.of(AppAction.EVERYTHING)),
+    TEACHER(EnumSet.of(AppAction.MANAGE_QUIZZES, AppAction.ASSESS_QUIZZES,
+            AppAction.SEND_NOTIFICATIONS)),
+    STUDENT(EnumSet.of(AppAction.ANSWER_QUIZZES));
+
+    private final Set<AppAction> permittedActions;
+
+    private UserRole(Set<AppAction> permittedActions) {
+        this.permittedActions = permittedActions;
+    }
+
+    public Set<AppAction> getPermittedActions() {
+        return permittedActions;
+    }
+}
