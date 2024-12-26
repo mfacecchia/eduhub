6ef26e0e73d7be3a2c94cbc4f381f859896aea2a
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-26 18:42:32 +0100

    feat: add class creation handler

--- backend/src/main/java/com/feis/eduhub/backend/features/systemClass/SystemClassController.java
+++ backend/src/main/java/com/feis/eduhub/backend/features/systemClass/SystemClassController.java
@@ -1,18 +1,50 @@
 package com.feis.eduhub.backend.features.systemClass;
 
+import java.util.EnumSet;
+
 import org.json.JSONObject;
 
+import com.feis.eduhub.backend.common.dto.ResponseDto;
+import com.feis.eduhub.backend.common.exceptions.UnauthorizedException;
 import com.feis.eduhub.backend.common.interfaces.EndpointsRegister;
+import com.feis.eduhub.backend.common.lib.MiddlewareExecutor;
+import com.feis.eduhub.backend.common.rbac.AppAction;
+import com.feis.eduhub.backend.common.rbac.Rbac;
+import com.feis.eduhub.backend.features.auth.AuthMiddleware;
 
 import io.javalin.Javalin;
+import io.javalin.http.Handler;
+import io.javalin.http.HandlerType;
 
 public class SystemClassController implements EndpointsRegister {
     private final String BASE_URL = "/class";
+    private final SystemClassService systemClassService = new SystemClassService();
+    private final Rbac rbac = new Rbac();
 
     @Override
     public void registerEndpoints(Javalin app) {
-        // TODO: Add implementation
+        // Auth middleware registering (POST method only)
+        app.before(EndpointsRegister.BASE_V1_ENDPOINT + BASE_URL,
+                MiddlewareExecutor.executeOnMethod(EnumSet.of(HandlerType.POST),
+                        AuthMiddleware.isLoggedIn(true, false, true)));
+
+        app.post(EndpointsRegister.BASE_V1_ENDPOINT + BASE_URL, createClass);
     }
+
+    private final Handler createClass = (ctx) -> {
+        JSONObject json = new JSONObject(ctx.body());
+        SystemClass systemClass = getClassFromBody(json);
+        boolean isAllowed = rbac.checkPermissionFromPersistence(ctx.attribute("accountId"), AppAction.MANAGE_CLASSES);
+        if (!isAllowed) {
+            throw new UnauthorizedException("Not authorized to complete this operation.");
+        }
+        systemClassService.createClass(systemClass);
+        ResponseDto<SystemClass> response = new ResponseDto.ResponseBuilder<SystemClass>(201)
+                .withMessage("Successfully created class")
+                .withData(systemClass)
+                .build();
+        ctx.status(201).json(response);
+    };
 
     private SystemClass getClassFromBody(JSONObject json) {
         return new SystemClass(
