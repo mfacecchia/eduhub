e90665edc889301d1b57fcaba9e6e879230c708e
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-27 00:44:58 +0100

    feat: add lesson DTO class

---
+++ backend/src/main/java/com/feis/eduhub/backend/features/lessonAttendance/dto/LessonDto.java
@@ -0,0 +1,18 @@
+package com.feis.eduhub.backend.features.lessonAttendance.dto;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class LessonDto {
+    private Long lessonId;
+    private Date lessonDate;
+    private Time startsAt;
+    private Time endsAt;
+    private Integer roomNo;
+    private final Boolean attended;
+}
