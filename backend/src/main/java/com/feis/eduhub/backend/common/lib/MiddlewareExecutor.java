7b44a260464d134446a1bbb0faffb2d7c0bc4a64
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-26 12:02:49 +0100

    feat: add middleware executor utility class

---
+++ backend/src/main/java/com/feis/eduhub/backend/common/lib/MiddlewareExecutor.java
@@ -0,0 +1,36 @@
+package com.feis.eduhub.backend.common.lib;
+
+import java.util.Set;
+
+import io.javalin.http.Handler;
+import io.javalin.http.HandlerType;
+
+/**
+ * Utility class for executing Javalin {@code Handler} middleware methods based
+ * on HTTP request method.
+ */
+public class MiddlewareExecutor {
+
+    /**
+     * Executes the given {@code middleware} only if the HTTP method of the current
+     * request matches any of the specified methods (or if the {@code methodsSet}
+     * set is empty (executes the mw for all methods)).
+     *
+     * @param methodsSet A {@code HandlerType} Set containing the HTTP methods
+     *                   for which the middleware should be executed
+     * @param middleware The middleware Handler to be executed
+     * @return A Handler that conditionally executes the middleware based on the
+     *         HTTP method
+     * 
+     * @see HandlerType
+     * @see Handler
+     */
+    public static Handler executeOnMethod(Set<HandlerType> methodsSet, Handler middleware) {
+        return (ctx) -> {
+            if (methodsSet.isEmpty()
+                    || methodsSet.contains(ctx.method())) {
+                middleware.handle(ctx);
+            }
+        };
+    }
+}
