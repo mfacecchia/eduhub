8b657722ca61fe298bac5de6e94d38294e335458
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-26 18:36:21 +0100

    feat(rbac): add app actions enumeration

---
+++ backend/src/main/java/com/feis/eduhub/backend/common/rbac/AppAction.java
@@ -0,0 +1,17 @@
+package com.feis.eduhub.backend.common.rbac;
+
+/**
+ * Enum representing all possible user actions within the app.
+ * The constant {@code EVERYTHING} represents the possibility for a service
+ * administrator to execute all actions
+ */
+public enum AppAction {
+    EVERYTHING,
+    MANAGE_QUIZZES,
+    ASSESS_QUIZZES,
+    ANSWER_QUIZZES,
+    MANAGE_ACCOUNTS,
+    MANAGE_CLASSES,
+    SEND_NOTIFICATIONS,
+    MANAGE_CLASS_MEMBERS,
+}
