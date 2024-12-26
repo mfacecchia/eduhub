45960f87985daed6b9bc9a2e01b62bdf7305c661
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-26 20:57:01 +0100

    feat: add option to include id in data from body obtaining method

--- backend/src/main/java/com/feis/eduhub/backend/features/systemClass/SystemClassUtility.java
+++ backend/src/main/java/com/feis/eduhub/backend/features/systemClass/SystemClassUtility.java
@@ -13,12 +13,17 @@ public final class SystemClassUtility {
     private SystemClassUtility() {
     }
 
-    public static SystemClass getClassFromBody(JSONObject json) {
-        return new SystemClass(
+    // TODO: Add option to not throw if field not found (useful for PUT requests)
+    public static SystemClass getClassFromBody(JSONObject json, boolean includeId) {
+        SystemClass systemClass = new SystemClass(
                 json.getString("courseName"),
                 json.getString("classAddress"),
                 json.getInt("classYear"),
                 json.getLong("teacherId"));
+        if (includeId) {
+            systemClass.setClassId(json.getLong("classId"));
+        }
+        return systemClass;
     }
 
     public static String getBaseUrl() {
