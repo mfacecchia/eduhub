f4e9d91f986894fc75156cb5e3c7988b4566d3b8
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-27 18:55:48 +0100

    feat: add account routes in main app

--- backend/src/main/java/com/feis/eduhub/backend/App.java
+++ backend/src/main/java/com/feis/eduhub/backend/App.java
@@ -3,6 +3,7 @@ package com.feis.eduhub.backend;
 import java.util.Arrays;
 
 import com.feis.eduhub.backend.common.config.AppBuilder;
+import com.feis.eduhub.backend.features.account.AccountController;
 import com.feis.eduhub.backend.features.auth.AuthController;
 import com.feis.eduhub.backend.features.systemClass.SystemClassController;
 
@@ -11,6 +12,7 @@ public class App {
         AppBuilder appBuilder = new AppBuilder(
                 Arrays.asList(
                         new AuthController(),
+                        new AccountController(),
                         new SystemClassController()));
         appBuilder.configureEndpoints(true);
         appBuilder.start();
