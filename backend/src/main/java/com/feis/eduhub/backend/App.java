a052abc293c294aaff966ed5165b7f368981196c
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-27 01:36:20 +0100

    feat: add classes endpoints in main app

--- backend/src/main/java/com/feis/eduhub/backend/App.java
+++ backend/src/main/java/com/feis/eduhub/backend/App.java
@@ -4,10 +4,15 @@ import java.util.Arrays;
 
 import com.feis.eduhub.backend.common.config.AppBuilder;
 import com.feis.eduhub.backend.features.auth.AuthController;
+import com.feis.eduhub.backend.features.systemClass.SystemClassController;
+import com.feis.eduhub.backend.features.systemClass.SystemClassMiddlewares;
 
 public class App {
     public static void main(String[] args) {
-        AppBuilder appBuilder = new AppBuilder(Arrays.asList(new AuthController()));
+        AppBuilder appBuilder = new AppBuilder(
+                Arrays.asList(
+                        new AuthController(),
+                        new SystemClassMiddlewares(), new SystemClassController()));
         appBuilder.configureEndpoints(true);
         appBuilder.start();
     }
