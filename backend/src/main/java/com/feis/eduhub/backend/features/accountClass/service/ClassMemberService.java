abc5a276e4f925508f12bf4dac5ce9a2779d17ef
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-27 23:01:43 +0100

    feat: add class member DTO service layer class

---
+++ backend/src/main/java/com/feis/eduhub/backend/features/accountClass/service/ClassMemberService.java
@@ -0,0 +1,38 @@
+package com.feis.eduhub.backend.features.accountClass.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import com.feis.eduhub.backend.common.config.DatabaseConnection;
+import com.feis.eduhub.backend.common.exceptions.AppException;
+import com.feis.eduhub.backend.common.exceptions.DataFetchException;
+import com.feis.eduhub.backend.features.accountClass.ClassMemberDao;
+import com.feis.eduhub.backend.features.accountClass.dto.ClassMemberDto;
+
+public class ClassMemberService {
+    private final ClassMemberDao classMemberDao;
+    DatabaseConnection databaseConnection;
+
+    public ClassMemberService() {
+        classMemberDao = new ClassMemberDao();
+        databaseConnection = DatabaseConnection.getInstance();
+    }
+
+    public List<ClassMemberDto> getAllClassMembers(long classId) throws AppException {
+        try (Connection conn = databaseConnection.getConnection()) {
+            return classMemberDao.getAll(classId, conn);
+        } catch (SQLException e) {
+            throw new DataFetchException("Could not fetch data", e);
+        }
+    }
+
+    public ClassMemberDto getSingleClassMember(long classId, long accountId) throws AppException {
+        try (Connection conn = databaseConnection.getConnection()) {
+            return classMemberDao.findByIds(Arrays.asList(classId, accountId), conn).get();
+        } catch (SQLException e) {
+            throw new DataFetchException("Could not fetch data", e);
+        }
+    }
+}
