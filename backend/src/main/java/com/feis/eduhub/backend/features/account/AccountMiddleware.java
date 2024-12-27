1b02631ee987a670318f3ab31245ee8b728154c1
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-27 18:13:57 +0100

    feat: implement rbac in account endpoints

--- backend/src/main/java/com/feis/eduhub/backend/features/account/AccountMiddleware.java
+++ backend/src/main/java/com/feis/eduhub/backend/features/account/AccountMiddleware.java
@@ -2,17 +2,23 @@ package com.feis.eduhub.backend.features.account;
 
 import java.util.EnumSet;
 
+import com.feis.eduhub.backend.common.exceptions.UnauthorizedException;
 import com.feis.eduhub.backend.common.interfaces.EndpointsRegister;
 import com.feis.eduhub.backend.common.lib.MiddlewareExecutor;
 import com.feis.eduhub.backend.common.middlewares.IsLoggedInMiddleware;
+import com.feis.eduhub.backend.common.rbac.AppAction;
+import com.feis.eduhub.backend.common.rbac.Rbac;
 
 import io.javalin.Javalin;
+import io.javalin.http.Handler;
 import io.javalin.http.HandlerType;
 
 public class AccountMiddleware implements EndpointsRegister {
     private final String BASE_URL = AccountUtility.getBaseUrl();
+    private final Rbac rbac;
 
     public AccountMiddleware() {
+        rbac = new Rbac();
     }
 
     @Override
@@ -21,5 +27,20 @@ public class AccountMiddleware implements EndpointsRegister {
                 MiddlewareExecutor.executeOnMethod(
                         EnumSet.allOf(HandlerType.class),
                         IsLoggedInMiddleware.isLoggedIn(true, false, true, false)));
+
+        app.before(EndpointsRegister.BASE_V1_ENDPOINT + BASE_URL,
+                MiddlewareExecutor.executeOnMethod(
+                        EnumSet.of(HandlerType.PUT, HandlerType.DELETE),
+                        isAllowed()));
+    }
+
+    private Handler isAllowed() {
+        return (ctx) -> {
+            boolean isAllowed = rbac.checkPermissionFromPersistence(ctx.attribute("accountId"),
+                    AppAction.MANAGE_ACCOUNTS);
+            if (!isAllowed) {
+                throw new UnauthorizedException("Not authorized to complete this operation.");
+            }
+        };
     }
 }
