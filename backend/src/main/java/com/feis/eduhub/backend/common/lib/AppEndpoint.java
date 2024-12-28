de93205eb9a196c30a354b58196e38f0d162a681
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-28 09:04:23 +0100

    feat: add app endpoints enumeration

---
+++ backend/src/main/java/com/feis/eduhub/backend/common/lib/AppEndpoint.java
@@ -0,0 +1,25 @@
+package com.feis.eduhub.backend.common.lib;
+
+/**
+ * Enumeration representing all available app endpoints which can be used by all
+ * controllers as a starting point to build their endpoints.
+ * The enum consists in a constant value representing the endpoint name and the
+ * actual endpoint (in form of String) as its value.
+ */
+public enum AppEndpoint {
+    DEFAULT_V1("/api/v1"),
+    AUTH("/auth"),
+    ACCOUNT("/account"),
+    CLASS("/class"),
+    LESSON("/lesson");
+
+    private final String BASE_URL;
+
+    private AppEndpoint(String baseUrl) {
+        BASE_URL = baseUrl;
+    }
+
+    public String getBaseUrl() {
+        return BASE_URL;
+    }
+}
