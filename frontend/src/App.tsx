effd828fd0d795c831a34a6247cb39bb456de6db
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-29 17:19:56 +0100

    feat: add query context provider in main app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
 import BackButton from "./components/common/BackButton";
 import MainLayout from "./layouts/MainLayout";
@@ -5,20 +6,24 @@ import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<MainLayout />}>
-                    <Route index element={<LandingPage />} />
-                </Route>
-                <Route path="login">
-                    <Route element={<BackButton />}>
-                        <Route index element={<LoginPage />} />
+            <QueryClientProvider client={queryClient}>
+                <Routes>
+                    <Route path="/" element={<MainLayout />}>
+                        <Route index element={<LandingPage />} />
                     </Route>
-                </Route>
-                <Route path="*" element={<NotFoundPage />}></Route>
-            </Routes>
+                    <Route path="login">
+                        <Route element={<BackButton />}>
+                            <Route index element={<LoginPage />} />
+                        </Route>
+                    </Route>
+                    <Route path="*" element={<NotFoundPage />}></Route>
+                </Routes>
+            </QueryClientProvider>
         </BrowserRouter>
     );
 }
