f877c38a8c7e88a007e56d422f123e1c016b38bb
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-29 14:42:33 +0100

    feat(MINOR): add back button in login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import BackButton from "./components/common/BackButton";
 import MainLayout from "./layouts/MainLayout";
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
@@ -9,6 +11,11 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainLayout />}>
                     <Route index element={<LandingPage />} />
+                </Route>
+                <Route path="login">
+                    <Route element={<BackButton />}>
+                        <Route index element={<LoginPage />} />
+                    </Route>
                 </Route>
                 <Route path="*" element={<NotFoundPage />}></Route>
             </Routes>
