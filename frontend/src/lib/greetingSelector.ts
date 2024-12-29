1662d9b48a0a8b12130c35f1670ecdf7aab86ef9
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-29 15:44:53 +0100

    feat: add greeting selector utility function

---
+++ frontend/src/lib/greetingSelector.ts
@@ -0,0 +1,29 @@
+type TGreetings = {
+    greeting: string;
+    timeRange: number[];
+};
+
+const greetings: TGreetings[] = [
+    {
+        greeting: "Good Morning",
+        timeRange: [5, 12],
+    },
+    {
+        greeting: "Good Afternoon",
+        timeRange: [12, 18],
+    },
+    {
+        greeting: "Good Evening",
+        timeRange: [18, 5],
+    },
+];
+
+export default function getGreeting() {
+    const time = new Date().getHours();
+    return greetings.find((timeGreeting) => {
+        return (
+            time > timeGreeting.timeRange[0] &&
+            time <= timeGreeting.timeRange[1]
+        );
+    })?.greeting;
+}
