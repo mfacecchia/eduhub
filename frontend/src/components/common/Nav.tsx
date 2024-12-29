37a32e584f7f11bc502adccb9b64ee7a8ac3a6be
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-29 09:29:29 +0100

    feat(component): add navbar component

---
+++ frontend/src/components/common/Nav.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router";
+import { Button } from "./Button";
+
+const Nav = () => {
+    return (
+        <nav className="w-full bg-background/85 border border-border rounded-[--radius] px-4 py-3 backdrop-blur-md sticky top-0 right-0 flex items-center justify-between">
+            <div className="flex items-center gap-3">
+                <h3>EduHub</h3>
+            </div>
+            <div>
+                <Link to="/login">
+                    <Button variant="link">Login</Button>
+                </Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
