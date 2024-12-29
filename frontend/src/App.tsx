37211a311edd6dd289a434a780c8972c8e76ee0f
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-29 11:11:49 +0100

    feat(routes): add app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import LandingPage from "./pages/LandingPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<LandingPage />} />
+                </Route>
+                <Route path="*" element={<NotFoundPage />}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
