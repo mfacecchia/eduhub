f3b04a6518a1dd8d9fabcca1347d429ae2f61ffc
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-29 11:11:23 +0100

    feat(page): add initial landing page components

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+    document.title = import.meta.env.VITE_PAGE_TITLE;
+
+    return (
+        <section>
+            <h1>Landing page</h1>
+        </section>
+    );
+};
+
+export default LandingPage;
