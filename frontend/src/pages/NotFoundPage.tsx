baa9b36d8743a4efe272b90fa1200f298d8d5e8f
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-29 11:05:59 +0100

    feat(page): add 404 page

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/common/Button";
+import Container from "@/components/common/Container";
+import NotFound from "@/components/icons/NotFound";
+import { LucideHome } from "lucide-react";
+import { NavLink } from "react-router";
+
+const NotFoundPage = () => {
+    document.title = import.meta.env.VITE_PAGE_TITLE + " - Not found";
+    return (
+        <Container>
+            <NotFound />
+            <div>
+                <h1>Uh oh!</h1>
+                <p className="muted">
+                    The page you're looking for seems to not exist.
+                </p>
+            </div>
+            <NavLink to="/" className="hover:animate-pulse mt-5">
+                <Button variant="link">
+                    Go back to
+                    <LucideHome className="inline align-text-bottom" />
+                    home
+                </Button>
+            </NavLink>
+        </Container>
+    );
+};
+
+export default NotFoundPage;
