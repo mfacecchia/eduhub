1df78ae8911236796b837759a9b1eb3b5f5e6f97
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-31 15:42:55 +0100

    feat(type): add account DB information data type definition

--- frontend/src/types/account.ts
+++ frontend/src/types/account.ts
@@ -2,3 +2,6 @@ import { accountSchema } from "@/schemas/accountSchema";
 import { z } from "zod";
 
 export type TAccount = z.infer<typeof accountSchema>;
+export type TDbAccount = {
+    id: number;
+} & Omit<TAccount, "password" | "repeatPassword">;
