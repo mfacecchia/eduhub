0c1ddc85d5f69f0f3d7a4169e2de1298c4543630
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-02 17:14:36 +0100

    feat: add support for collection objects in preparedStatement params setting

--- backend/src/main/java/com/feis/eduhub/backend/common/lib/Sql.java
+++ backend/src/main/java/com/feis/eduhub/backend/common/lib/Sql.java
@@ -2,6 +2,7 @@ package com.feis.eduhub.backend.common.lib;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -14,19 +15,32 @@ public class Sql {
      * Sets all the parameters in the passed {@code PreparedStatement}.
      * 
      * @param ps     the prepared statement to set the parameters to
-     * @param params all the parameters list
+     * @param params all the parameters list.
      * @throws SQLException if parameterIndex does not correspond to a parameter
      *                      marker in the SQL statement; if a database access error
      *                      occurs;
      *                      this method is called on a closed
      *                      {@code PreparedStatement}
      *                      or the type of the given object is ambiguous
+     * @apiNote if you want to pass a list of params in the {@code params} list, it
+     *          is possible to do so by passing another instance of any
+     *          {@code Collection}.
+     *          Passing arrays as value will possibly result in unintended behavior.
      * 
      * @see PreparedStatement
      */
     public static void setParams(PreparedStatement ps, List<? extends Object> params) throws SQLException {
-        for (int i = 0; i < params.size(); i++) {
-            ps.setObject(i + 1, params.get(i));
+        int paramPositionCounter = 1;
+        for (Object param : params) {
+            if (param instanceof Collection paramsList) {
+                for (Object listParam : paramsList) {
+                    ps.setObject(paramPositionCounter, listParam);
+                    paramPositionCounter++;
+                }
+                continue;
+            }
+            ps.setObject(paramPositionCounter, param);
+            paramPositionCounter++;
         }
     }
 }
