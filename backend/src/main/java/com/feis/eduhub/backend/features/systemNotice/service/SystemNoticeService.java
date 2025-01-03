9a54fa35f446e5fd90c29472d4a811d4a938461c
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-03 22:25:43 +0100

    feat: add notice sending method

--- backend/src/main/java/com/feis/eduhub/backend/features/systemNotice/service/SystemNoticeService.java
+++ backend/src/main/java/com/feis/eduhub/backend/features/systemNotice/service/SystemNoticeService.java
@@ -4,10 +4,13 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import com.feis.eduhub.backend.common.config.DatabaseConnection;
+import com.feis.eduhub.backend.common.config.MailSender;
 import com.feis.eduhub.backend.common.exceptions.AppException;
 import com.feis.eduhub.backend.common.exceptions.DatabaseQueryException;
+import com.feis.eduhub.backend.common.exceptions.EmailSendingException;
 import com.feis.eduhub.backend.features.systemNotice.SystemNotice;
 import com.feis.eduhub.backend.features.systemNotice.dao.SystemNoticeDao;
+import com.feis.eduhub.backend.features.systemNotice.template.NoticeTemplate;
 
 public class SystemNoticeService {
     private final SystemNoticeDao systemNoticeDao;
@@ -35,5 +38,24 @@ public class SystemNoticeService {
         } catch (SQLException e) {
             throw new DatabaseQueryException("Error while deleting notice", e);
         }
+    }
+
+    /**
+     * Sends a notice to the specified user user via email through
+     * the {@code MailSender} class.
+     *
+     * @param recipient The email address of the recipient
+     * @param message   The content of the notice to be sent
+     * @throws EmailSendingException If there is an error during the email sending
+     *                               process
+     * 
+     * @see MailSender
+     * @see NoticeTemplate
+     */
+    public void sendNoticeViaEmail(String recipient, String message) throws EmailSendingException {
+        MailSender mailSender = MailSender.getInstance("EduHub Mailing System");
+        String fullHtmlMessage = NoticeTemplate.renderHtmlNotice(message);
+        mailSender.deliverMessage(recipient, "There's a message for you.", "Hello",
+                fullHtmlMessage, false);
     }
 }
