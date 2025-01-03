af20e9f3562ed9a0ddea1a287022e57908367c18
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-03 14:45:18 +0100

    feat(MINOR): add lesson date formatting

--- backend/src/main/java/com/feis/eduhub/backend/features/lessonAttendance/dto/LessonDto.java
+++ backend/src/main/java/com/feis/eduhub/backend/features/lessonAttendance/dto/LessonDto.java
@@ -3,6 +3,8 @@ package com.feis.eduhub.backend.features.lessonAttendance.dto;
 import java.sql.Date;
 import java.sql.Time;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,6 +12,7 @@ import lombok.Data;
 @Data
 public class LessonDto {
     private Long lessonId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date lessonDate;
     private Time startsAt;
     private Time endsAt;
