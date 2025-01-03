11c4e571826ccf6b3d057afc0b427899ca7121c6
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-03 19:20:11 +0100

    feat: add notices list obtaining handler

--- backend/src/main/java/com/feis/eduhub/backend/features/systemNotice/controller/SystemNoticeController.java
+++ backend/src/main/java/com/feis/eduhub/backend/features/systemNotice/controller/SystemNoticeController.java
@@ -1,12 +1,16 @@
 package com.feis.eduhub.backend.features.systemNotice.controller;
 
+import java.util.List;
+
 import org.json.JSONObject;
 
 import com.feis.eduhub.backend.common.dto.ResponseDto;
 import com.feis.eduhub.backend.common.interfaces.EndpointsRegister;
 import com.feis.eduhub.backend.common.lib.ContextUtil;
 import com.feis.eduhub.backend.features.systemNotice.SystemNotice;
+import com.feis.eduhub.backend.features.systemNotice.dto.SystemNoticeFullInfoDto;
 import com.feis.eduhub.backend.features.systemNotice.middleware.SystemNoticeMiddleware;
+import com.feis.eduhub.backend.features.systemNotice.service.SystemNoticeFullInfoService;
 import com.feis.eduhub.backend.features.systemNotice.service.SystemNoticeService;
 import com.feis.eduhub.backend.features.systemNotice.util.SystemNoticeUtility;
 
@@ -17,18 +21,33 @@ public class SystemNoticeController implements EndpointsRegister {
     private final String BASE_V1_ENDPOINT = SystemNoticeUtility.getBaseUrl();
     private final SystemNoticeService systemNoticeService;
     private final SystemNoticeMiddleware systemNoticeMiddleware;
+    private final SystemNoticeFullInfoService systemNoticeFullInfoService;
 
     public SystemNoticeController() {
         systemNoticeMiddleware = new SystemNoticeMiddleware();
         systemNoticeService = new SystemNoticeService();
+        systemNoticeFullInfoService = new SystemNoticeFullInfoService();
     }
 
     @Override
     public void registerEndpoints(Javalin app) {
         systemNoticeMiddleware.registerEndpoints(app);
 
+        app.get(BASE_V1_ENDPOINT, noticesListHandler());
         app.post(BASE_V1_ENDPOINT, createNoticeHandler());
         app.delete(BASE_V1_ENDPOINT + "/{noticeId}", deleteNoticeHandler());
+    }
+
+    private Handler noticesListHandler() {
+        return (ctx) -> {
+            List<SystemNoticeFullInfoDto> noticesList = systemNoticeFullInfoService.getAllNotices();
+            ResponseDto<List<SystemNoticeFullInfoDto>> response = new ResponseDto.ResponseBuilder<List<SystemNoticeFullInfoDto>>(
+                    200)
+                    .withMessage(String.format("Found %d entries", noticesList.size()))
+                    .withData(noticesList)
+                    .build();
+            ctx.status(201).json(response);
+        };
     }
 
     private Handler createNoticeHandler() {
