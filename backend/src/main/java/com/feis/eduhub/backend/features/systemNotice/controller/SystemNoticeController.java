eab12f2755470b02d4a8574ee267b47e98e6ac7d
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-03 17:43:37 +0100

    WIP: add notice controller layer class

---
+++ backend/src/main/java/com/feis/eduhub/backend/features/systemNotice/controller/SystemNoticeController.java
@@ -0,0 +1,23 @@
+package com.feis.eduhub.backend.features.systemNotice.controller;
+
+import com.feis.eduhub.backend.common.interfaces.EndpointsRegister;
+import com.feis.eduhub.backend.features.systemNotice.middleware.SystemNoticeMiddleware;
+import com.feis.eduhub.backend.features.systemNotice.service.SystemNoticeService;
+import com.feis.eduhub.backend.features.systemNotice.util.SystemNoticeUtility;
+
+import io.javalin.Javalin;
+
+public class SystemNoticeController implements EndpointsRegister {
+    private final String BASE_V1_ENDPOINT = SystemNoticeUtility.getBaseUrl();
+    private final SystemNoticeService systemNoticeService;
+    private final SystemNoticeMiddleware systemNoticeMiddleware;
+
+    public SystemNoticeController() {
+        systemNoticeMiddleware = new SystemNoticeMiddleware();
+        systemNoticeService = new SystemNoticeService();
+    }
+
+    @Override
+    public void registerEndpoints(Javalin app) {
+    }
+}
