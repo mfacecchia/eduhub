908d0eb2bea038097f5fdd8174d6e6b66678061a
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-03 11:43:43 +0100

    feat: add utility class for lesson model

---
+++ backend/src/main/java/com/feis/eduhub/backend/features/lesson/LessonUtility.java
@@ -0,0 +1,31 @@
+package com.feis.eduhub.backend.features.lesson;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import org.json.JSONObject;
+
+/**
+ * Utility class providing helper methods for Lesson operations.
+ * 
+ * @see Lesson
+ */
+public class LessonUtility {
+
+    private LessonUtility() {
+    }
+
+    public static Lesson getLessonFromBody(JSONObject json, boolean includeId) {
+        Lesson lesson = new Lesson(
+                new Date(json.optLong("lessonDate")),
+                new Time(json.optLong("startsAt")),
+                new Time(json.optLong("endsAt")),
+                json.optIntegerObject("roomNo"),
+                json.optLongObject("createdById"),
+                json.optLongObject("classId"));
+        if (includeId) {
+            lesson.setLessonId(json.optLongObject("lessonId"));
+        }
+        return lesson;
+    }
+}
