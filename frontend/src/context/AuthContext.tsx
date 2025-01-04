022694ef59076ebd4676291bfa4d630c4dd0119f
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-04 18:26:51 +0100

    refactor: remove redirect mechanism & add context type definition

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,40 +1,26 @@
 import { useAccountQuery } from "@/hooks/queries";
 import { TDbAccount } from "@/types/account";
 import { createContext, ReactNode, useContext } from "react";
-import { useNavigate } from "react-router";
 
-const AccountAuthContext = createContext<TDbAccount | undefined>(undefined);
+type TAuthContext = {
+    account: TDbAccount | undefined;
+    isLoading: boolean;
+};
+
+const AccountAuthContext = createContext<TAuthContext>({} as TAuthContext);
 
 const AuthContext = ({ children }: { children: ReactNode }) => {
-    const navigate = useNavigate();
     const { data: account, isLoading } = useAccountQuery();
 
-    if (!isLoading && !account) {
-        if (
-            window.location.pathname !== "/login" &&
-            window.location.pathname !== "/"
-        ) {
-            navigate("/login");
-        }
-    }
-    if (account && window.location.pathname === "/login") {
-        navigate("/dashboard");
-    }
-
     return (
-        <AccountAuthContext.Provider value={account}>
+        <AccountAuthContext.Provider value={{ account, isLoading }}>
             {children}
         </AccountAuthContext.Provider>
     );
 };
 
 function useAccountAuthContext() {
-    const context = useContext(AccountAuthContext);
-    if (!context)
-        throw new Error(
-            "Context must be used inside a AccountAuthContext component"
-        );
-    return context;
+    return useContext(AccountAuthContext);
 }
 
 export default AuthContext;
