e71097e834898030dfe500674486dbf5698767c9
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-04 13:24:16 +0100

    feat: add account login type definition

--- frontend/src/types/account.ts
+++ frontend/src/types/account.ts
@@ -2,6 +2,9 @@ import { accountSchema } from "@/schemas/accountSchema";
 import { z } from "zod";
 
 export type TAccount = z.infer<typeof accountSchema>;
+
+export type TLoginAccount = Pick<TAccount, "email" | "password">;
+
 export type TDbAccount = {
     id: number;
 } & Omit<TAccount, "password" | "repeatPassword">;
