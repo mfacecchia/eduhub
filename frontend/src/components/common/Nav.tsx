a0eb125102b3b85d5f7d7d959eef9737cb5a0179
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-04 18:27:38 +0100

    feat: add account avatar in navbar

--- frontend/src/components/common/Nav.tsx
+++ frontend/src/components/common/Nav.tsx
@@ -1,7 +1,12 @@
+import { useAccountAuthContext } from "@/context/AuthContext";
+import { getAccountInitials } from "@/lib/accountInitials";
 import { Link } from "react-router";
+import { Avatar, AvatarFallback } from "./Avatar";
 import { Button } from "./Button";
 
 const Nav = () => {
+    const { account } = useAccountAuthContext();
+
     return (
         <nav className="w-full bg-background/85 border border-border rounded-[--radius] px-4 py-3 backdrop-blur-md flex items-center justify-between mb-7">
             <div className="flex items-center gap-3">
@@ -11,7 +16,18 @@ const Nav = () => {
             </div>
             <div>
                 <Link to="/login">
-                    <Button variant="link">Login</Button>
+                    {account ? (
+                        <Avatar className="size-8">
+                            <AvatarFallback className="bg-muted-foreground text-muted text-xs">
+                                {getAccountInitials(
+                                    account.firstName,
+                                    account.lastName
+                                )}
+                            </AvatarFallback>
+                        </Avatar>
+                    ) : (
+                        <Button variant="link">Login</Button>
+                    )}
                 </Link>
             </div>
         </nav>
